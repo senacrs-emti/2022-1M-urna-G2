6e73409c301e5c7ef0372d902b930d4476153902
IPreHandaI <alicegomeseberhardt@gmail.com>
2022-09-22 12:25:28 -0300

    commit terminal mesario 22.09

---
+++ terminal-mesario/terminal-mesario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="urna1">
+    <div class="urna-topo">
+        <div id="biometria"></div>
+    </div>
+    </div>
+
+    <div class="urna-tela">
+        <div id="tela"></div>
+    </div>
+
+    <div class="urna-teclado">
+        <div id=""></div>
+    </div>
+
+
+</body>
+</html>
