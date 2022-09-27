6638434a6b87965d84f23735cb53fac55b32a3b0
juliaguima <92822185+juliaguima@users.noreply.github.com>
2022-09-27 12:21:54 -0300

    Tabela admin 1.0

---
+++ admin/candidatos-lista.php
@@ -0,0 +1,215 @@
+<?php
+
+include_once './includes/_head.php';
+include_once './includes/_header.php';
+
+
+?>
+
+<body>
+
+
+<table id="tabela"class="table table-striped">
+<thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Partido</th>
+      <th scope="col">Número</th>
+      <th scope="col">Cargo</th>
+      <th scope="col">Imagem</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Marcos</td>
+      <td>YUPI</td>
+      <td>3562</td>
+      <td>Deputado Federal</td>
+      <td>marcos.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>PIPI</td>
+      <td>8989</td>
+      <td>Deputado Federal</td>
+      <td>Jacob.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Paulo</td>
+      <td>FGAR</td>
+      <td>1563</td>
+      <td>Deputado Federal</td>
+      <td>Paulo.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Marcos</td>
+      <td>YUPI</td>
+      <td>3562</td>
+      <td>Deputado Federal</td>
+      <td>marcos.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Jacob</td>
+      <td>PIPI</td>
+      <td>8989</td>
+      <td>Deputado Federal</td>
+      <td>Jacob.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Paulo</td>
+      <td>FGAR</td>
+      <td>1563</td>
+      <td>Deputado Federal</td>
+      <td>Paulo.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Marcos</td>
+      <td>YUPI</td>
+      <td>3562</td>
+      <td>Deputado Federal</td>
+      <td>marcos.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Jacob</td>
+      <td>PIPI</td>
+      <td>8989</td>
+      <td>Deputado Federal</td>
+      <td>Jacob.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Paulo</td>
+      <td>FGAR</td>
+      <td>1563</td>
+      <td>Deputado Federal</td>
+      <td>Paulo.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Marcos</td>
+      <td>YUPI</td>
+      <td>3562</td>
+      <td>Deputado Federal</td>
+      <td>marcos.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Jacob</td>
+      <td>PIPI</td>
+      <td>8989</td>
+      <td>Deputado Federal</td>
+      <td>Jacob.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>Paulo</td>
+      <td>FGAR</td>
+      <td>1563</td>
+      <td>Deputado Federal</td>
+      <td>Paulo.png</td>
+
+    </tr>
+    </tr>
+    <tr>
+      <th scope="row">13</th>
+      <td>Marcos</td>
+      <td>YUPI</td>
+      <td>3562</td>
+      <td>Deputado Federal</td>
+      <td>marcos.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">14</th>
+      <td>Jacob</td>
+      <td>PIPI</td>
+      <td>8989</td>
+      <td>Deputado Federal</td>
+      <td>Jacob.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">15</th>
+      <td>Paulo</td>
+      <td>FGAR</td>
+      <td>1563</td>
+      <td>Deputado Federal</td>
+      <td>Paulo.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">16</th>
+      <td>Marcos</td>
+      <td>YUPI</td>
+      <td>3562</td>
+      <td>Deputado Federal</td>
+      <td>marcos.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">17</th>
+      <td>Jacob</td>
+      <td>PIPI</td>
+      <td>8989</td>
+      <td>Deputado Federal</td>
+      <td>Jacob.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">18</th>
+      <td>Paulo</td>
+      <td>FGAR</td>
+      <td>1563</td>
+      <td>Deputado Federal</td>
+      <td>Paulo.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">19</th>
+      <td>Marcos</td>
+      <td>YUPI</td>
+      <td>3562</td>
+      <td>Deputado Federal</td>
+      <td>marcos.png</td>
+
+    </tr>
+    <tr>
+      <th scope="row">20</th>
+      <td>Jacob</td>
+      <td>PIPI</td>
+      <td>8989</td>
+      <td>Deputado Federal</td>
+      <td>Jacob.png</td>
+
+    </tr>  
+  </tbody>
+</table>
+
+<?php
+
+include_once './includes/_footer.php';
+
+
+
+?>
---
+++ admin/includes/_header.php
@@ -0,0 +1,7 @@
+<header>
+
+
+<img src="./content/senac.png" width="253" height="150" alt="">
+
+</header>
+
