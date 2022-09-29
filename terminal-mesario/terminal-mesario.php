e7d01ca8f1c0413f5bea8381a4003b8a2c4fc8c6
Framezinho <alicegomeseberhardt@gmail.com>
2022-09-29 12:07:44 -0300

    commit 29.09

--- terminal-mesario/terminal-mesario.php
+++ terminal-mesario/terminal-mesario.php
@@ -23,28 +23,62 @@
     <div class="urna">
     <div class="urna-teclado">
 
+    <div class="teclas">
     <div>
-        <div class="teclado">1</div>
-        <div class="teclado">4</div>
-        <div class="teclado">7</div>
-        <div id="confirma">confirma</div>
+        <button class="teclado">1</button>
+        <button class="teclado">4</button>
+        <button class="teclado">7</button>
+        <button id="confirma">confirma</button>
     </div>
 
     <div>
-        <div class="teclado">2</div>
-        <div class="teclado">5</div>
-        <div class="teclado">8</div>
-        <div class="teclado">0</div>
+        <button class="teclado">2</button>
+        <button class="teclado">5</button>
+        <button class="teclado">8</button>
+        <button class="teclado">0</button>
     </div>
 
     <div>
-        <div class="teclado">3</div>
-        <div class="teclado">6</div>
-        <div class="teclado">9</div>
-        <div id="corrige">corrige</div>
+        <button class="teclado">3</button>
+        <button class="teclado">6</button>
+        <button class="teclado">9</button>
+        <button id="corrige">corrige</button>
     </div>
 
-    <aside id="luzes"></aside>
+    </div>
+
+    <div id="sinais">
+
+        <div id="escrito">
+            <div class="text">
+                <p>Segurança</p>
+                <span style='background-color: red;' class='ponto'></span>
+            </div>
+            <div class="text">
+                <p>Bateria interna</p>
+                <span style='background-color: red;' class='ponto'></span>
+            </div>
+            <div class="text">
+                <p>Aguarde</p>
+                <span style='background-color: yellow;' class='ponto'></span>
+            </div>
+            <div class="text">
+                <p>Liberado</p>
+                <span style='background-color: green;' class='ponto'></span>
+            </div>
+            <!-- <div class="luz">Bateria interna</div>
+            <div class="luz">Aguarde</div>
+            <div class="luz">Liberado</div> -->
+        </div>
+
+        <!-- <div id="luzes">
+        <div class="vermelho"></div>
+        <div class="vermelho"></div>
+        <div class="amarelo"></div>
+        <div class="verde"></div>
+        </div> -->
+
+    </div>
 
     </div>
     </div>
